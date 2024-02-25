cf647ef627234fe2e95b7378b35eecaa08916c6f
Lantos1618 <l.leong1618@gamil.com>
2024-02-25 17:13:46 +0000

    Refactor main function to use a loop and add printf statement

--- src/main.ll
+++ src/main.ll
@@ -1,14 +1,26 @@
-; ModuleID = 'main'
-source_filename = "main"
+; ModuleID = 'my_module'
+source_filename = "my_module"
 
-define i32 @main(i32 %0) {
-main:
-  %a = alloca i32, align 4
-  store i32 %0, ptr %a, align 4
-  %b = alloca i32, align 4
-  %add_left = load i32, ptr %a, align 4
-  %add = add i32 %add_left, 10
-  store i32 %add, ptr %b, align 4
-  %b1 = load i32, ptr %b, align 4
-  ret i32 %b1
+declare i32 @printf(i8*, ...)
+
+define i64 @main() {
+entry:
+  %x = alloca i64, align 8
+  store i64 42, i64* %x, align 8
+  br label %loop
+
+loop:                                             ; preds = %loop, %entry
+  %x1 = load i64, i64* %x, align 8
+  %inc = add i64 %x1, 1
+  store i64 %inc, i64* %x, align 8
+  %lttmp = icmp slt i64 %inc, 100
+  br i1 %lttmp, label %loop, label %afterloop
+
+afterloop:                                        ; preds = %loop
+  %x2 = load i64, i64* %x, align 8
+  %fmt = getelementptr [5 x i8], [5 x i8]* @.str, i32 0, i32 0
+  call i32 (i8*, ...) @printf(i8* %fmt, i64 %x2)
+  ret i64 %x2
 }
+
+@.str = private unnamed_addr constant [5 x i8] c"%ld\0A\00", align 1
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::execution_engine::{ExecutionEngine, JitFunction};
 use inkwell::module::{Linkage, Module};
+use inkwell::types::{BasicType, BasicTypeEnum, FunctionType};
 use inkwell::values::{BasicValue, BasicValueEnum, PointerValue};
 use inkwell::{AddressSpace, OptimizationLevel};
 
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::Binary;
 use std::ops::Deref;
 
 type Identifier = String;
@@ -70,9 +72,35 @@ struct Block {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    Ne,
+    Lt,
+    Lte,
+    Gt,
+    Gte,
+    And,
+    Or,
+    
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct BinaryExpr {
+    op: BinaryOp,
+    lhs: Box<Expr>,
+    rhs: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 enum Expr {
     Literal(Literal),
     Identifier(Identifier),
+    BinaryExpr(BinaryExpr),
     
     Assignment(Assignment),
 
@@ -96,14 +124,27 @@ struct CodeGen<'ctx> {
     execution_engine: ExecutionEngine<'ctx>,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+struct Symbol<'ctx> {
+    pointer: PointerValue<'ctx>,
+    pointee_type: BasicTypeEnum<'ctx>,
+}
+
 impl<'ctx> CodeGen<'ctx> {
-    fn codegen_expr( &mut self, mut symbol_table: HashMap<Identifier, PointerValue<'ctx>>, expr: &Expr) -> Result<Option<BasicValueEnum<'ctx>>, Box<dyn Error>> {
+    fn codegen_expr(&mut self, symbol_table: &mut HashMap<Identifier, Symbol<'ctx>>, expr: &Expr) -> Result<Option<BasicValueEnum<'ctx>>, Box<dyn Error>> {
         match expr {
 
             Expr::FuncDef(func_def) => {
      
                 let param_types = vec![];
-                let func = self.context.i32_type().fn_type(&param_types, false);
+                let func = match func_def.return_type {
+                    TypeExpr::Int => self.context.i64_type().fn_type(&param_types, false),
+                    TypeExpr::Float => self.context.f64_type().fn_type(&param_types, false),
+                    TypeExpr::String => self.context.i8_type().ptr_type(AddressSpace::default()).fn_type(&param_types, false),
+                    TypeExpr::Bool => self.context.bool_type().fn_type(&param_types, false),
+                    TypeExpr::Void => self.context.void_type().fn_type(&param_types, false),                       
+                    _ => unimplemented!()
+                }; 
 
                 let function_name = func_def.name.as_str();
 
@@ -115,12 +156,62 @@ impl<'ctx> CodeGen<'ctx> {
 
                 // generate the function body
 
-                let body = self.codegen_expr(symbol_table.clone(), &*func_def.body)?; 
+                let body = self.codegen_expr(symbol_table, &*func_def.body)?; 
 
 
                 Ok(None)
-            }
+            },
+            Expr::BinaryExpr(binary)  => {
+                let lhs_val = self.codegen_expr(symbol_table, &binary.lhs)?.unwrap();
+                let rhs_val = self.codegen_expr(symbol_table, &binary.rhs)?.unwrap();
 
+                // check if the types are the same
+                if lhs_val.get_type() != rhs_val.get_type() {
+                    return Err("Types do not match".into());
+                }
+
+                    //    match type int, float 
+
+
+
+                if(lhs_val.is_int_value()){ 
+                    let result = match binary.op {
+                        BinaryOp::Add => self.builder.build_int_add(lhs_val.into_int_value(), rhs_val.into_int_value(), "addtmp"),
+                        BinaryOp::Sub => self.builder.build_int_sub(lhs_val.into_int_value(), rhs_val.into_int_value(), "subtmp"),
+                        BinaryOp::Mul => self.builder.build_int_mul(lhs_val.into_int_value(), rhs_val.into_int_value(), "multmp"),
+                        BinaryOp::Div => self.builder.build_int_signed_div(lhs_val.into_int_value(), rhs_val.into_int_value(), "divtmp"),
+                        BinaryOp::Mod => self.builder.build_int_signed_rem(lhs_val.into_int_value(), rhs_val.into_int_value(), "modtmp"),
+                        BinaryOp::Eq => self.builder.build_int_compare(inkwell::IntPredicate::EQ, lhs_val.into_int_value(), rhs_val.into_int_value(), "eqtmp"),
+                        BinaryOp::Ne => self.builder.build_int_compare(inkwell::IntPredicate::NE, lhs_val.into_int_value(), rhs_val.into_int_value(), "netmp"),
+                        BinaryOp::Lt => self.builder.build_int_compare(inkwell::IntPredicate::SLT, lhs_val.into_int_value(), rhs_val.into_int_value(), "lttmp"),
+                        BinaryOp::Lte => self.builder.build_int_compare(inkwell::IntPredicate::SLE, lhs_val.into_int_value(), rhs_val.into_int_value(), "ltetmp"),
+                        BinaryOp::Gt => self.builder.build_int_compare(inkwell::IntPredicate::SGT, lhs_val.into_int_value(), rhs_val.into_int_value(), "gttmp"),
+                        BinaryOp::Gte => self.builder.build_int_compare(inkwell::IntPredicate::SGE, lhs_val.into_int_value(), rhs_val.into_int_value(), "gtetmp"),
+                        BinaryOp::And => self.builder.build_and(lhs_val.into_int_value(), rhs_val.into_int_value(), "andtmp"),
+                        BinaryOp::Or => self.builder.build_or(lhs_val.into_int_value(), rhs_val.into_int_value(), "ortmp"),
+                    };
+                    return Ok(Some(result.unwrap().into()));
+                }
+                // if (lhs_val.is_float_value()) {
+                //     let result = match binary.op {
+                //         BinaryOp::Add => self.builder.build_float_add(lhs_val.into_float_value(), rhs_val.into_float_value(), "addtmp"),
+                //         BinaryOp::Sub => self.builder.build_float_sub(lhs_val.into_float_value(), rhs_val.into_float_value(), "subtmp"),
+                //         BinaryOp::Mul => self.builder.build_float_mul(lhs_val.into_float_value(), rhs_val.into_float_value(), "multmp"),
+                //         BinaryOp::Div => self.builder.build_float_div(lhs_val.into_float_value(), rhs_val.into_float_value(), "divtmp"),
+                //         BinaryOp::Mod => self.builder.build_float_rem(lhs_val.into_float_value(), rhs_val.into_float_value(), "modtmp"),
+                //         // BinaryOp::Eq => self.builder.build_float_compare(inkwell::FloatPredicate::OEQ, lhs_val.into_float_value(), rhs_val.into_float_value(), "eqtmp"),
+                //         // BinaryOp::Ne => self.builder.build_float_compare(inkwell::FloatPredicate::ONE, lhs_val.into_float_value(), rhs_val.into_float_value(), "netmp"),
+                //         // BinaryOp::Lt => self.builder.build_float_compare(inkwell::FloatPredicate::OLT, lhs_val.into_float_value(), rhs_val.into_float_value(), "lttmp"),
+                //         // BinaryOp::Lte => self.builder.build_float_compare(inkwell::FloatPredicate::OLE, lhs_val.into_float_value(), rhs_val.into_float_value(), "ltetmp"),
+                //         // BinaryOp::Gt => self.builder.build_float_compare(inkwell::FloatPredicate::OGT, lhs_val.into_float_value(), rhs_val.into_float_value(), "gttmp"),
+                //         // BinaryOp::Gte => self.builder.build_float_compare(inkwell::FloatPredicate::OGE, lhs_val.into_float_value(), rhs_val.into_float_value(), "gtetmp"),
+                //         // BinaryOp::And => self.builder.build_and(lhs_val.into_int_value(), rhs_val.into_int_value(), "andtmp"),
+                //         // BinaryOp::Or => self.builder.build_or(lhs_val.into_int_value(), rhs_val.into_int_value(), "ortmp"),
+                //         _ => unimplemented!()
+                //     }?;
+                // }
+                Ok(None)
+            }
             Expr::Literal(literal) => match literal {
                 Literal::Int(int) => {
                     let int_type = self.context.i64_type();
@@ -148,50 +239,98 @@ impl<'ctx> CodeGen<'ctx> {
                 // if not found, return an error
 
                 if (symbol_table.contains_key(name)) {
-                    let pointer_value = match symbol_table.get(name) {
-                        Some(pointer_value) => pointer_value,
+                    let symbol = match symbol_table.get(name) {
+                        Some(sym) => sym,
                         None => return Err("Variable not found".into()),
                     };
-                    let pointer_type = pointer_value.get_type();
+
+                    let pointer_value = symbol.pointer;
+                    let pointee_type = symbol.pointee_type;
 
                     let value = self
                         .builder
-                        .build_load(pointer_type, *pointer_value, name)?;
-
+                        .build_load(pointee_type, pointer_value, name)?;
+                    // dump the value
                     Ok(Some(value))
+
                 } else {
                     Err("Variable not found".into())
                 }
             }
+            Expr::IfExpr(IfExpr) => {
+                let condition = self.codegen_expr(symbol_table, &IfExpr.condition)?.unwrap();
+                let condition = self.builder.build_int_compare(inkwell::IntPredicate::NE, condition.into_int_value(), self.context.i64_type().const_zero(), "ifcond")?;
+                let function = self.builder.get_insert_block().unwrap().get_parent().unwrap();
+                let then_block = self.context.append_basic_block(function, "then");
+                let else_block = self.context.append_basic_block(function, "else");
+                let merge_block = self.context.append_basic_block(function, "ifcont");
 
+                self.builder.build_conditional_branch(condition, then_block, else_block);
+
+                self.builder.position_at_end(then_block);
+                let then_val = self.codegen_expr(symbol_table, &IfExpr.then_expr)?.unwrap();
+                self.builder.build_unconditional_branch(merge_block);
+                let then_block = self.builder.get_insert_block().unwrap();
+
+                self.builder.position_at_end(else_block);
+                let else_val = self.codegen_expr(symbol_table, &IfExpr.else_expr)?.unwrap();
+                self.builder.build_unconditional_branch(merge_block);
+                let else_block = self.builder.get_insert_block().unwrap();
+
+                self.builder.position_at_end(merge_block);
+                let phi = self.builder.build_phi(self.context.i64_type(), "iftmp")?;
+                phi.add_incoming(&[(&then_val, then_block), (&else_val, else_block)]);
+                Ok(Some(phi.as_basic_value()))
+            }
+
+            Expr::LoopExpr(loop_expr) => {
+                let loop_block = self.context.append_basic_block(self.builder.get_insert_block().unwrap().get_parent().unwrap(), "loop");
+                let after_block = self.context.append_basic_block(self.builder.get_insert_block().unwrap().get_parent().unwrap(), "afterloop");
+
+                self.builder.build_unconditional_branch(loop_block);
+                self.builder.position_at_end(loop_block);
+
+                let condition = self.codegen_expr(symbol_table, &loop_expr.condition)?.unwrap();
+                let condition = self.builder.build_int_compare(inkwell::IntPredicate::NE, condition.into_int_value(), self.context.i64_type().const_zero(), "loopcond")?;
+                self.builder.build_conditional_branch(condition, loop_block, after_block);
+
+                self.builder.position_at_end(after_block);
+
+                Ok(None)
+            }
             Expr::Block(block) => {
+                let mut temp_table = symbol_table.clone();
                 for expr in &block.exprs {
-                    self.codegen_expr(symbol_table.clone(), expr)?;
+                    self.codegen_expr(&mut temp_table, expr)?;
                 }
                 Ok(None)
             }
            
-            // In Expr::Assignment
             Expr::Assignment(assignment) => {
                 let identifier = &assignment.name;
-                let rhs_val = self.codegen_expr(symbol_table.clone(), &assignment.value)?.unwrap();
+                let rhs_val = self.codegen_expr(symbol_table, &assignment.value)?.unwrap();
 
-                 let val = match symbol_table.clone().get(identifier) {
-                    Some(val) => {
-                        let res = self.builder.build_store(*val, rhs_val)?;
+                 let val: Result<Option<BasicValueEnum<'_>>, Box<dyn Error>> = match symbol_table.get(identifier) {
+                    Some(symbol) => {
+                        let ptr = symbol.pointer;
+                        let res = self.builder.build_store(ptr, rhs_val)?;
                         Ok(None)
                     }
                     None => {
-                        let alloca = self.builder.build_alloca(rhs_val.get_type(), identifier)?;
-                        let res = self.builder.build_store(alloca, rhs_val)?;
-                        symbol_table.insert(identifier.to_string(), alloca);
+                        let ptr = self.builder.build_alloca(rhs_val.get_type(), identifier)?;
+
+                        let res = self.builder.build_store(ptr, rhs_val)?;
+                        symbol_table.insert(identifier.to_string(), Symbol { pointer: ptr, pointee_type: rhs_val.get_type() }); 
                         Ok(None)
                     }
                 };
                 val
             }
             Expr::Return(expr) => {
-                let return_val = self.codegen_expr(symbol_table.clone(), expr)?.unwrap();
+   
+                let return_val = self.codegen_expr(symbol_table, expr)?.unwrap();
+
+
                 self.builder.build_return(Some(&return_val));
                 Ok(None)
             }
@@ -217,11 +356,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         builder: context.create_builder(),
         execution_engine,
     };
-    let mut symbol_table = HashMap::new();
-
+    let mut symbol_table = HashMap::<Identifier, Symbol>::new();
     let main_ast = Expr::FuncDef(FuncDef {
         name: "main".to_string(),
-        return_type: TypeExpr::Void,
+        return_type: TypeExpr::Int,
         args: vec![],
         body: Box::new(Expr::Block(
             Block {
@@ -230,13 +368,33 @@ fn main() -> Result<(), Box<dyn Error>> {
                         name: "x".to_string(),
                         value: Box::new(Expr::Literal(Literal::Int(42))),
                     }),
+                    // loop(x < 100) { x = x + 1 }
+                    Expr::LoopExpr(LoopExpr {
+                        condition: Box::new(Expr::BinaryExpr(BinaryExpr {
+                            op: BinaryOp::Lt,
+                            lhs: Box::new(Expr::Identifier("x".to_string())),
+                            rhs: Box::new(Expr::Literal(Literal::Int(100))),
+                        })),
+                        body: Box::new(Expr::Block(Block {
+                            exprs: vec![Expr::Assignment(Assignment {
+                                name: "x".to_string(),
+                                value: Box::new(Expr::FuncCall {
+                                    name: "+".to_string(),
+                                    args: vec![
+                                        Expr::Identifier("x".to_string()),
+                                        Expr::Literal(Literal::Int(1)),
+                                    ],
+                                }),
+                            })],
+                        })),
+                    }),
                     Expr::Return(Box::new(Expr::Identifier("x".to_string()))),
                 ],
             }
         )),
     });
 
-    codegen.codegen_expr(symbol_table, &main_ast)?;
+    codegen.codegen_expr(&mut symbol_table, &main_ast)?;
 
     codegen.dump();
 
